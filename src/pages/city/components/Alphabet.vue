ce9290307ed5b897a69646cf05b332367899c712
zdh <zdh@qq.com>
2018-11-29 23:20:09 +0800

    city layout

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alphabet">
+    <ul>
+      <li class="item">A</li>
+      <li class="item">B</li>
+      <li class="item">B</li>
+      <li class="item">B</li>
+      <li class="item">B</li>
+      <li class="item">B</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .alphabet
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 2.6rem
+    right 0
+    width 0.4rem
+    .item
+      line-height 0.44rem
+      font-size 0.3rem
+      text-align center
+      color $themeColor
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="wrapper">
+    <div class="areas">
+      <div class="list-title border-topbottom">您的位置</div>
+      <div class="list-wrapper">
+        <ul>
+          <li class="button-wrapper"><div class="button">龙岩</div></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="areas">
+      <div class="list-title border-topbottom">热门城市</div>
+      <div class="list-wrapper">
+        <ul>
+          <li class="button-wrapper"><div class="button">龙岩</div></li>
+          <li class="button-wrapper"><div class="button">龙岩</div></li>
+          <li class="button-wrapper"><div class="button">龙岩</div></li>
+          <li class="button-wrapper"><div class="button">龙岩</div></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="areas">
+      <div class="list-title border-topbottom">A</div>
+      <div class="city-wrapper">
+        <ul>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+        </ul>
+      </div>
+    </div>
+    <div class="areas">
+      <div class="list-title border-topbottom">A</div>
+      <div class="city-wrapper">
+        <ul>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+        </ul>
+      </div>
+    </div>
+    <div class="areas">
+      <div class="list-title border-topbottom">A</div>
+      <div class="city-wrapper">
+        <ul>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+        </ul>
+      </div>
+    </div>
+    <div class="areas">
+      <div class="list-title border-topbottom">A</div>
+      <div class="city-wrapper">
+        <ul>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+          <li class="city-item border-bottom">阿拉尔</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="stylus" scoped>
+  .wrapper
+    position absolute
+    top 1.6rem
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
+    .areas
+      .list-title
+        line-height .7rem
+        padding-left .25rem
+        font-size .24rem
+        color #212121
+        background-color #f5f5f5
+      .list-wrapper
+        padding .1rem .9rem .1rem .2rem
+        overflow hidden
+        .button-wrapper
+          float left
+          width 33.33%
+          .button
+            margin 0.1rem
+            padding 0.1rem 0
+            text-align center
+            border .02rem solid #ccc
+            border-radius .04rem
+      .city-wrapper
+        .city-item
+          line-height 0.76rem
+          padding-left 0.2rem
+</style>
